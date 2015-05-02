786512953ca0db0cef913ae9c85bafcddda3ef21
Amie Kweon <amie.kweon@rd.io>
2015-05-01 21:53:46 -0700

    Add assets

---
+++ SwiftTW33Tme/Views/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  SwiftTW33Tme
+//
+//  Created by Amie Kweon on 5/1/15.
+//  Copyright (c) 2015 Rdio. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
