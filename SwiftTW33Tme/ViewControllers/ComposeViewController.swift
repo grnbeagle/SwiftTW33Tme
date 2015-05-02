81e7edd071f760ff95a33f053d6522ad29f58f95
Amie Kweon <amie.kweon@rd.io>
2015-05-02 01:53:05 -0700

    Add compose view

---
+++ SwiftTW33Tme/ViewControllers/ComposeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ComposeViewController.swift
+//  SwiftTW33Tme
+//
+//  Created by Amie Kweon on 5/2/15.
+//  Copyright (c) 2015 Rdio. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func onClose(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
